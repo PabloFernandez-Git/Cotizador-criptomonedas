8471504284673a3bd7152abcace12eb31ca6c139
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-07-21 13:09:41 -0300

    Styled Component para el Error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import useMoneda from '../hooks/useMoneda';
-import useCriptomoneda from '../hooks/useCriptomoneda'
+import useCriptomoneda from '../hooks/useCriptomoneda';
 import axios from 'axios';
+import Error from './Error';
 
 
 const Boton = styled.input`
@@ -73,7 +74,7 @@ const Formulario = () => {
             onSubmit={cotizarMoneda}
         >
 
-            {error ? 'Hay un error' : null}
+            {error ? <Error mensaje="Todos los campos son obligatorios"/> : null}
             
             <SelectMonedas />
 
