a9f8670f90489dcaf4a3ddb2c141c53eb31e4395
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-07-22 14:59:11 -0300

    Consultando la API con la cotización

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import imagen from './cryptomonedas.png';
 import Formulario from './components/Formulario';
+import axios from 'axios';
 
 
 const Contenedor = styled.div`
@@ -46,10 +47,19 @@ function App() {
   // useEffect
   useEffect(() => {
 
-    // Evitamos la ejecucion la primera vez
-    if (moneda === '') return;
+    const cotizarCriptomoneda = async() => {
 
-    console.log('cotizando')
+      // Evitamos la ejecucion la primera vez
+      if (moneda === '') return;
+
+      // Consultar la API para obtener la cotizacion
+      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+      const resultado = await axios.get(url);
+
+      console.log(resultado.data.DISPLAY[criptomoneda][moneda]);
+    }
+    cotizarCriptomoneda();
 
   }, [moneda, criptomoneda]);
 
