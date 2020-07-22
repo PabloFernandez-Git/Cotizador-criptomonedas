5c77a529c1b962d1842e782fcc7b0a2950470963
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-07-22 16:41:48 -0300

    CSS al Resumen

--- src/components/Cotizacion.jsx
+++ src/components/Cotizacion.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ResultadoDiv = styled.div`
+    color: #fff;
+    font-family: Arial, Helvetica, sans-serif;
+`
+
+const Info = styled.p`
+    font-size: 18px;
+
+    span {
+        font-weight: bold;
+    }
+`
+
+const Precio = styled.p`
+    font-size: 30px;
+
+    span {
+        font-weight: bold;
+    }
+`
 
 const Cotizacion = ({resultado}) => {
     
     if(Object.keys(resultado).length === 0) return null;
 
     return (  
-        <div>
-            <p>El precio es: <span>{resultado.PRICE}</span></p>
-            <p>Precio mas alto del dia: <span>{resultado.HIGHDAY}</span></p>
-            <p>Precio mas bajo del dia: <span>{resultado.LOWDAY}</span></p>
-            <p>Variacion ultimas 24hs: <span>{resultado.CHANGEPCT24HOUR}</span></p>
-            <p>Ultima actualizacion: <span>{resultado.LASTUPDATE}</span></p>
-        </div>
+        <ResultadoDiv>
+            <Precio>El precio es: <span>{resultado.PRICE}</span></Precio>
+            <Info>Precio mas alto del dia: <span>{resultado.HIGHDAY}</span></Info>
+            <Info>Precio mas bajo del dia: <span>{resultado.LOWDAY}</span></Info>
+            <Info>Variacion ultimas 24hs: <span>{resultado.CHANGEPCT24HOUR}</span></Info>
+            <Info>Ultima actualizacion: <span>{resultado.LASTUPDATE}</span></Info>
+        </ResultadoDiv>
     );
 }
 
